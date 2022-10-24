38ef634163bfe835b959d28cffd9bc2ae08240a2
justamitsaha <justamitsaha@gmail.com>
2022-10-24 11:52:05 +0530

    Add product validation

--- product-services/src/main/java/com/saha/amit/productServices/DTO/ProductRequestDTO.java
+++ product-services/src/main/java/com/saha/amit/productServices/DTO/ProductRequestDTO.java
@@ -5,12 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductRequestDTO {	
+public class ProductRequestDTO {
+	@NotEmpty(message = "Please provide product name")
 	private String name;
+	@NotEmpty(message = "Please provide product description")
 	private String description;
+	@NotEmpty(message = "Please provide product prise")
 	private Double prise;
 }
