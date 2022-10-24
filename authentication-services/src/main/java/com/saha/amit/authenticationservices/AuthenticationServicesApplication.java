f674ec246035dc7f26f250f97b52bff1caa7ded7
justamitsaha <justamitsaha@gmail.com>
2022-10-24 21:52:29 +0530

    Adding Authentication service to Api gateway and discovery service

--- authentication-services/src/main/java/com/saha/amit/authenticationservices/AuthenticationServicesApplication.java
+++ authentication-services/src/main/java/com/saha/amit/authenticationservices/AuthenticationServicesApplication.java
@@ -2,6 +2,7 @@ package com.saha.amit.authenticationservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class AuthenticationServicesApplication {
     public static void main(String[] args) {
         System.out.println("http://localhost:8080/authentication/h2-console/");
